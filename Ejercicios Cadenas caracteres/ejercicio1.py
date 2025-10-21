5ff1b9ad82c7aaff64235cce2869d2734298e0b7
Raul Ibanez (IZV) <ribanez310@ieszaidinvergeles.org>
2025-10-21 13:21:01 +0200

    Python

---
+++ Ejercicios Cadenas caracteres/ejercicio1.py
@@ -0,0 +1,7 @@
+# Escribir un programa que pregunte el nombre del usuario en la consola y un número entero e imprima por pantalla en líneas 
+# distintas el nombre del usuario tantas veces como el número introducido.
+
+nombre_usuario = int(input("Introduzca nombre de usuario: "))
+numero_entero = int(input("Introduzca numero entero: "))
+
+operacion = nombre_usuario
